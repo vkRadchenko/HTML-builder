fa9d7084c5b7108ec1d95f72680be8c2167ebcd1
Vladimir Radchenko <vk.rad4enko@gmail.com>
2024-01-25 16:20:58 +0300

    fix: change asinc logic and folder creation check

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,22 +1,22 @@
-const { readdir, readFile, writeFile } = require('fs/promises');
-const fs = require('fs');
+const fs = require('fs').promises;
+const { createReadStream, createWriteStream } = require('fs');
 const path = require('path');
 
 const distPath = path.join(__dirname, 'project-dist');
 const stylesPath = path.join(__dirname, 'styles');
-const bindleFile = path.join(distPath, 'bundle.css');
+const bundleFile = path.join(distPath, 'bundle.css');
 
 const mergeStyle = async () => {
+  await fs.mkdir(distPath, { recursive: true });
+  const readStyleFolder = await fs.readdir(stylesPath);
+  const output = createWriteStream(bundleFile);
 
-  const readStyleFolder = await readdir(stylesPath);
-  const output = fs.createWriteStream(bindleFile);
-  
   readStyleFolder.reduce(async (acc, fail) => {
     const pathFile = path.join(stylesPath, fail);
     const elStats = path.extname(pathFile);
 
     if (elStats === '.css') {
-      const input = fs.createReadStream(pathFile);
+      const input = createReadStream(pathFile);
 
       let data = [];
 
@@ -26,7 +26,6 @@ const mergeStyle = async () => {
       input.on('end', () => console.log('End', data.length));
       input.on('error', (error) => console.log('Error', error.message));
       input.on('data', (chunk) => output.write(chunk));
-
     }
   }, 0);
 };
